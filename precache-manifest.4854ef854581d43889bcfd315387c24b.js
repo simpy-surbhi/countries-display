44368ade2327b9478c270f88c9c595f1f06cc33a
amit kumar <amit@preeostudios.com>
2022-04-10 00:18:15 +0200

    Updates

---
+++ precache-manifest.4854ef854581d43889bcfd315387c24b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c0d063ae9bf50b3dc616bf08695d50",
+    "url": "/countries-display/index.html"
+  },
+  {
+    "revision": "4b631f8589baeeaa4105",
+    "url": "/countries-display/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c499b657160a6790580c",
+    "url": "/countries-display/static/js/2.923e3152.chunk.js"
+  },
+  {
+    "revision": "fde8a65b8d307872c8c44f6de8ee07de",
+    "url": "/countries-display/static/js/2.923e3152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b631f8589baeeaa4105",
+    "url": "/countries-display/static/js/main.5a59391f.chunk.js"
+  },
+  {
+    "revision": "3d358bbc8c18b1fa8b34",
+    "url": "/countries-display/static/js/runtime-main.c1a10aea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18d37fd97fec745d5664faad1c92ea24.js"
+  "/countries-display/precache-manifest.4854ef854581d43889bcfd315387c24b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/countries-display/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
